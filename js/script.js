f194f4990885eb7644a2f938ba6b5957b6baab28
Joao Fontes <joaofontes@aluno-3030.wireless.ua.pt>
2024-12-02 11:08:36 +0000

    5º tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 //ficheiro de script
+
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+        event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+});
